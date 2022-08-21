e8a3b4128c8d3d160a4526de27884c633b8d079b
Annalyssa Stakes <annalyssastakes@Annalyssas-Air.attlocal.net>
2022-08-21 14:07:46 -0500

    completed graded assignment

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
 using System.IO;
 using System.Reflection;
 using System.Text;
@@ -49,7 +51,7 @@ namespace TechJobsConsole
             {
                 string aValue = row[column];
 
-                if (aValue.Contains(value))
+                if (aValue.ToLower().Contains(value.ToLower()))
                 {
                     jobs.Add(row);
                 }
@@ -138,5 +140,33 @@ namespace TechJobsConsole
 
             return rowValues.ToArray();
         }
+
+        public static List<Dictionary<string, string>> FindByValue(string value)
+        {
+            LoadData();
+
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
+
+            foreach (Dictionary<string, string> job in AllJobs)
+            {
+
+                foreach (KeyValuePair<string, string> column in job)
+                {
+                    string aValue = column.Value;
+
+                    if (aValue.ToLower().Contains(value.ToLower()))
+                    {
+                        if (!jobs.Contains(job))
+                        {
+                            jobs.Add(job);
+
+                        }
+                    }
+
+                }
+            }
+            return jobs;
+        }
     }
 }
+            
